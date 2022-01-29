143df9fdc07f8d69bb37edc30b2b323be3858ebb
ravikiran <ravikiransjce.jobs@gmail.com>
2022-01-29 09:07:58 +0530

    Added news and news item class based components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import NavBar from "./components/NavBar";
+import NewsComponent from "./components/NewsComponent";
+
+export default class App extends Component {
+  name = "ravikiran";
+  render() {
+    return (
+      <div>
+        <NavBar></NavBar>
+        <NewsComponent></NewsComponent>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/NewsItemComponent.js
@@ -0,0 +1,24 @@
+import { getByTitle } from "@testing-library/react";
+import React, { Component } from "react";
+
+export class NewsItemComponent extends Component {
+  render() {
+    let { title, description, imgUrl, newsUrl } = this.props;
+    return (
+      <div className='my-3'>
+        <div className='card' style={{ width: "18rem" }}>
+          <img src={imgUrl} className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>{title}</h5>
+            <p className='card-text'>{description}</p>
+            <a href='/newsdetail' className='btn btn-primary btn-sm'>
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItemComponent;
