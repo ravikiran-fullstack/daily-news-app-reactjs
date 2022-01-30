e66e7faf3d774b0c9cfcc06b4b85ba2d8d6ece43
ravikiran <ravikiransjce.jobs@gmail.com>
2022-01-30 12:43:05 +0530

    Added code for fetching news using fetch api, and added pagination

--- src/components/NewsItemComponent.js
+++ src/components/NewsItemComponent.js
@@ -7,11 +7,15 @@ export class NewsItemComponent extends Component {
     return (
       <div className='my-3'>
         <div className='card' style={{ width: "18rem" }}>
-          <img src={imgUrl} className='card-img-top' alt='...' />
+          <img src={imgUrl} className='card-img-top newsCardImg' alt='...' />
           <div className='card-body'>
-            <h5 className='card-title'>{title}</h5>
-            <p className='card-text'>{description}</p>
-            <a href='/newsdetail' className='btn btn-primary btn-sm'>
+            <h5 className='card-title titleText'>{title}</h5>
+            <p className='card-text descriptionText'>{description}</p>
+            <a
+              href={newsUrl}
+              target='_blank'
+              className='btn btn-primary btn-sm'
+            >
               Read more
             </a>
           </div>
